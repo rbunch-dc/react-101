344b67a54c634a8ea978d5f7b4ab9e83f5519154
Robert Bunch <rob@digitalcrafts.com>
2017-01-03 16:27:19 -0500

    Add coin flip

--- scoreboard.jsx
+++ scoreboard.jsx
@@ -1,5 +1,10 @@
-// This app sucks. There is no point in useing React. Everything is hardcoded. So far...
-// Let's make some JSON!
+// State. V1.
+// Props are immutable (they do not change!)
+// State is a piece of data, that does change.
+// State also has an initial value, that changes when X happens
+// In the case of the National Championship, it starts at 0... initial state
+// Some kind of mutation or change occurs.
+// In order to use state, we have to make our functions, React classes
 
 var teams = [
 	{
@@ -31,32 +36,60 @@ function Team(props){
 	return(
 		<div className="team">
 			<div className="team-name">{props.name}</div>
-			<div className="counter">
-				<button className="counter-change minus">-</button>
-				<div className="team-score"> {props.score} </div>
-				<button className="counter-change plus">+</button>
-			</div>
+			{/* Remove Counter. Make it a componenet */ }
+			<Counter score={props.score} />
 		</div>
 	)
 }
+
+// React.createClass is a React component class that takes an object
+// The only REQUIRED proprety is a render method
+// EVERYTIME state changes, render gets called
+var Counter = React.createClass(
+	{
+		// We also need to add a getInitialState property that is part of React
+		getInitialState: function() {
+			// this function sets the initial state of a variable. 
+			// It usually returns a single object
+			return {
+				time: "60:00",
+				score: 0
+			}
+		},
+		losePoint: function(){
+			console.log("You clicked on the down button!!!")
+			// setState is a special React function that updates the state
+			this.setState({
+				score: (this.state.score - 1)
+			})
+		},
+		gainPoint: function(){
+			this.setState({
+				score: (this.state.score + 1)
+			})			
+		},
+		render: function(){
+			// back to where we are used to!!
+			return(
+				<div className="counter">
+					<button onClick={this.losePoint} className="counter-change minus">-</button>
+					{/* Change the score from this.props.score to state! */}
+					<div className="team-score"> {this.state.score} </div>
+					<button onClick={this.gainPoint} className="counter-change plus">+</button>
+				</div>			
+			)
+		}
+	}
+);
 
 function Application(props){
 	return(
 		<div className="scoreboard">
 			<Header title={props.title} />
 			<div className="teams">
-
-
 				{/* Using Map to loop through our teams JSON. These are no longer neccessary */ }
-				{/* <Team name="Alabama" score="0" /> */ }
-				{/* <Team name="Clemson" score="0" /> */ }
-
-				{/* props.teams is an array or team objects */}	
-				{/* return a component with the current team name and score */}	
-				{/* () => {} is the ES6 way of writing function(){} */}	
 
 				{props.teams.map((team, index) =>{
-					{/* Add a key so that React can quickly and efficiently manage the DOM */}	
 					return <Team key={index} name={team.name} score={team.score} />
 				})}
 
@@ -67,6 +100,17 @@ function Application(props){
 
 
 ReactDOM.render(
-	<Application title="National Championship Scoreboard" teams={teams} />,
+	<Application cartoonCharacter="buggs" title="National Championship Scoreboard" teams={teams} />,
 	document.getElementById('container')
 )
+
+
+// var anObj = {
+// 	render: function(){
+// 		console.log('Hello');
+// 	}
+
+// }
+
+
+// anObj.render()
